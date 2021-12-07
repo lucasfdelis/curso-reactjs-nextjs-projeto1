fd8f4b6bd8a39f306b8835700bbf7b90a8ff5a0b
Lucas Alves <lucas@colegiopanorama.com.br>
2021-12-07 15:41:15 -0300

    Lifecycle methods (aulas 16-17)

--- src/App.js
+++ src/App.js
@@ -1,45 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
 import { Component } from 'react';
-import { render } from '@testing-library/react';
 
 class App extends Component {
-  
-  state = {
-    name: 'Lucas Alves',
-    counter: 0
-  };
-  
-  handlePClick = () => {
-    this.setState({name: 'Junior'})
-  }
 
-  handleAClick = (event) => {
-    event.preventDefault();
-    const { counter } = this.state;
-    this.setState({ counter: counter+1});
+  state = {
+    posts: []
+  };
+
+  componentDidMount(){
+    setTimeout(() => {
+      this.setState({
+        posts: [
+          {
+            id: 1,
+            title: 'O título 1',
+            body: 'O corpo 1'
+          },
+          {
+            id: 2,
+            title: 'O título 2',
+            body: 'O corpo 2'
+          },
+          {
+            id: 3,
+            title: 'O título 3',
+            body: 'O corpo 3'
+          },
+        ]
+      })
+    })
   }
 
   render(){
-    const { name, counter } = this.state;
+    const { posts } = this.state;
 
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p onClick={this.handlePClick}>
-            {name} {counter}
-          </p>
-          <a
-            onClick={this.handleAClick}
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Este é o link
-          </a>
-        </header>
+        {posts.map(post => (
+          <div>
+          <h1 key={post.id}>{post.title}</h1>
+          <p>{post.body}</p>
+          </div>
+        ))}
       </div>
     );
   }
