037bbc9dfc80bab2bf459570a5da137050107f82
Lucas Alves <lucas@colegiopanorama.com.br>
2021-12-07 14:58:02 -0300

    Aulas 1-13

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import logo from './logo.svg';
+import './App.css';
+import { Component } from 'react';
+import { render } from '@testing-library/react';
+
+class App extends Component {
+  
+  state = {
+    name: 'Lucas Alves',
+    counter: 0
+  };
+  
+  handlePClick = () => {
+    this.setState({name: 'Junior'})
+  }
+
+  handleAClick = (event) => {
+    event.preventDefault();
+    const { counter } = this.state;
+    this.setState({ counter: counter+1});
+  }
+
+  render(){
+    const { name, counter } = this.state;
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p onClick={this.handlePClick}>
+            {name} {counter}
+          </p>
+          <a
+            onClick={this.handleAClick}
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Este é o link
+          </a>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
